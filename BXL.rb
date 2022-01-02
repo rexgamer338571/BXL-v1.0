2dccdb1b3110e415d1cd6c9b63aa46d3efd76a68
rexgamer338571 <wojciechm225@gmail.com>
2022-01-02 22:50:15 +0100

    Added README.md

---
+++ BXL.rb
@@ -0,0 +1,36 @@
+module BXL
+    def self.interprete(x)
+        x.each_line{|line|
+            a = line.gsub("\n","").gsub("\t","").gsub("\r","").split(" ")
+            command = a.shift
+            args = a
+            case command
+            when "outln"
+                print "#{args[0]}\n"
+            when "ln"
+                print "\n"*args[0].to_i
+            when "chprint"
+                print "#{args[0]}"*args[1].to_i
+            when "pause"
+                $stdin.gets
+            when "end"
+                exit
+            when "wait"
+                sleep args[1].to_f
+            when "help"
+                print "outln - prints the text\nln - creates new line\nchprint - prints one char\npause - pause\nend - exits"
+            else
+                raise("ERROR")
+            end
+        }
+    end
+
+    def self.interprete_file(x)
+        data = lambda{File.open(x,'rb'){|f|return f.read}}.call
+        BXL.interprete(data)
+    end
+end
+
+begin
+    BXL.interprete_file(ARGV[0].to_s)
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# Bentax Programming Language v1.0
+## How to run
+
+cmd command (requires Ruby interpreter installed): `ruby BXL.rb filename.bx`
+
+## All v1.0 commands
+
+outln <text> - prints the text
+
+end - exits the program
+
+pause - pause
+
+chprint <char> <count> - prints one char
+
+ln <count> - creates new line(s)
